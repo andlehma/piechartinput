e858793bab5e6f08290c7d213b02f417f270d6f5
Andrew <andrew.lehman364@gmail.com>
2019-11-09 13:26:27 -0600

    convert to custom html element

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-// variable declarations
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = canvas.height = 300;
-const center = canvas.width / 2;
-const radius = center - 10;
-const handleRad = 6;
 const pi = Math.PI;
 const pi2 = pi * 2;
-ctx.lineWidth = 2;
-
 const mouse = {
     x: innerWidth / 2,
     y: innerHeight / 2
-};
-
-// helper functions
-addEventListener('mousemove',
-    function (event) {
-        mouse.x = event.clientX;
-        mouse.y = event.clientY;
-    });
-
-addEventListener('mousedown',
-    function () {
-        mouse.down = true;
-    });
-
-addEventListener('mouseup',
-    function () {
-        mouse.down = false;
-    });
-
-function getMouseAngle() {
-    let r = distance(mouse.x, mouse.y, center, center);
-    let relativeMouseX = (mouse.x - center) / r;
-    let relativeMouseY = (-mouse.y + center) / r;
-    let mouseAngle = Math.atan2(relativeMouseY, relativeMouseX);
-    if (mouseAngle < 0) mouseAngle += pi2;
-    return (mouseAngle);
 };
 
 function distance(x1, y1, x2, y2) {
@@ -47,18 +12,46 @@ function distance(x1, y1, x2, y2) {
     return d;
 };
 
-function getPosFromAngle(angle) {
-    let xPos = center + radius * Math.cos(angle);
-    let yPos = center - radius * Math.sin(angle);
-    return ({ x: xPos, y: yPos });
-}
+class pieInput extends HTMLElement {
+    constructor() {
+        // dom setup
+        super();
+        this.shadow = this.attachShadow({ mode: 'open' });
+        this.canvas = document.createElement('canvas');
+        this.canvas.setAttribute('class', 'pie-input-canvas');
+        this.shadow.appendChild(this.canvas);
 
-class pieInput {
-    constructor(percents, radius, initialAngle = 0) {
-        this.percents = percents;
-        this.initialAngle = initialAngle;
-        this.radius = radius;
+        if (this.hasAttribute('values')){
+            this.percents = this.getAttribute('values').split(',').map(Number);
+        } else {
+            this.percents = [.5, .5];
+        }
+
+        if (this.hasAttribute('size')) {
+            this.canvas.width = this.canvas.height = parseInt(this.getAttribute('size'));
+        } else {
+            this.canvas.width = this.canvas.height = 300;
+        }
+
+        if (this.hasAttribute('initial-angle')) {
+            this.initialAngle = parseInt(this.getAttribute('initial-angle'));
+        } else {
+            this.initialAngle = 0;
+        }
+
+        // initialize variables
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.lineWidth = 2;
+        this.center = this.canvas.width / 2;
+        this.radius = this.center - 10;
+        this.handleRad = .04 * this.radius;
         this.angles = [];
+        let oldAngle = this.initialAngle;
+        for (let i = 0; i < this.percents.length; i++) {
+            let angle = (this.percents[i] * pi2) + oldAngle;
+            this.angles.push(angle);
+            oldAngle = angle;
+        }
         this.mouseOver = new Array(this.percents.length).fill(false);
         this.grab = new Array(this.percents.length).fill(false);
         this.globalGrab = false;
@@ -67,18 +60,43 @@ class pieInput {
         let valid = this.percents.reduce((a, b) => a + b, 0) === 1;
         if (!valid) console.error('pie chart input: percent array must sum to 1');
 
+        // listeners
+        window.addEventListener('mousemove',
+            function (event) {
+                mouse.x = event.clientX;
+                mouse.y = event.clientY;
+            });
+
+        window.addEventListener('mousedown',
+            function () {
+                mouse.down = true;
+            });
+
+        window.addEventListener('mouseup',
+            function () {
+                mouse.down = false;
+            });
+
         // bind
         this.animate = this.animate.bind(this);
+
+        // init
+        this.animate();
     }
 
-    getAngles() {
-        this.angles = [];
-        let oldAngle = this.initialAngle;
-        for (let i = 0; i < this.percents.length; i++) {
-            let angle = (this.percents[i] * pi2) + oldAngle;
-            this.angles.push(angle);
-            oldAngle = angle;
-        }
+    getMouseAngle() {
+        let r = distance(mouse.x, mouse.y, this.center, this.center);
+        let relativeMouseX = (mouse.x - this.center) / r;
+        let relativeMouseY = (-mouse.y + this.center) / r;
+        let mouseAngle = Math.atan2(relativeMouseY, relativeMouseX);
+        if (mouseAngle < 0) mouseAngle += pi2;
+        return (mouseAngle);
+    };
+
+    getPosFromAngle(angle) {
+        let xPos = this.center + this.radius * Math.cos(angle);
+        let yPos = this.center - this.radius * Math.sin(angle);
+        return ({ x: xPos, y: yPos });
     }
 
     getPercentsFromAngles(newAngles) {
@@ -104,16 +122,16 @@ class pieInput {
 
         for (let i = 0; i < this.angles.length; i++) {
             // draw line for each percent
-            ctx.beginPath();
-            ctx.moveTo(center, center);
-            let linePos = getPosFromAngle(this.angles[i]);
-            ctx.lineTo(linePos.x, linePos.y);
-            ctx.stroke();
+            this.ctx.beginPath();
+            this.ctx.moveTo(this.center, this.center);
+            let linePos = this.getPosFromAngle(this.angles[i]);
+            this.ctx.lineTo(linePos.x, linePos.y);
+            this.ctx.stroke();
 
             // draw handle
-            ctx.beginPath();
-            ctx.arc(linePos.x, linePos.y, handleRad, 0, pi2);
-            ctx.fill();
+            this.ctx.beginPath();
+            this.ctx.arc(linePos.x, linePos.y, this.handleRad, 0, pi2);
+            this.ctx.fill();
 
             // check for mouse over each handle
             let d = distance(mouse.x, mouse.y, linePos.x, linePos.y);
@@ -138,12 +156,14 @@ class pieInput {
 
             if (this.grab[i]) {
                 let newAngles = this.angles.slice();
-                newAngles[i] = getMouseAngle();
+                newAngles[i] = this.getMouseAngle();
 
                 let newPercents = this.getPercentsFromAngles(newAngles);
-                if (newPercents){
+                if (newPercents) {
                     this.angles = newAngles;
                     this.percents = newPercents.slice();
+                    console.log(this.percents);
+                    this.setAttribute('values', String(this.percents));
                 }
             }
         }
@@ -151,17 +171,15 @@ class pieInput {
 
     animate() {
         requestAnimationFrame(this.animate);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         // draw outline circle
-        ctx.beginPath();
-        ctx.arc(center, center, this.radius, 0, pi2);
-        ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.arc(this.center, this.center, this.radius, 0, pi2);
+        this.ctx.stroke();
 
         this.update();
     }
-}
+};
 
-let myChart = new pieInput([.3, .3, .4], radius);
-myChart.getAngles();
-myChart.animate();
+customElements.define('pie-input', pieInput);
